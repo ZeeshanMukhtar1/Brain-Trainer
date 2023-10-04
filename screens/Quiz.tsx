dd91c299d600a15ad3f685fb4c9a35d3f0204ed3
Zeeshan Mukhtar <Zeshanmukhtar878@gmail.com>
2023-10-04 13:54:36 -0700

    Feat : added logic r quiz

--- screens/Quiz.tsx
+++ screens/Quiz.tsx
@@ -1,47 +1,84 @@
-import {
-  StyleSheet,
-  Text,
-  Touchable,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+interface Question {
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
 
 export default function Quiz({navigation}: {navigation: any}) {
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const getQuiz = async () => {
+    const url =
+      'https://opentdb.com/api.php?amount=10&category=18&difficulty=hard&type=multiple';
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.results[0]);
+    setQuestions(data.results);
+  };
+
+  useEffect(() => {
+    getQuiz();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.top}>
-        <Text style={styles.Question}> Q : Question 1</Text>
-      </View>
-      <View style={styles.options}>
-        <TouchableOpacity style={styles.optionButton}>
-          <Text style={styles.option}>Answer 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.optionButton}>
-          <Text style={styles.option}>Answer 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.optionButton}>
-          <Text style={styles.option}>Answer 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.optionButton}>
-          <Text style={styles.option}>Answer 4</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.bottom}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Skip</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Next</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            navigation.navigate('Results');
-          }}>
-          <Text style={styles.buttonText}>End</Text>
-        </TouchableOpacity>
-      </View>
+      {/* Check if questions are available */}
+      {questions.length > 0 ? (
+        <View style={styles.parent}>
+          {/* Question section */}
+          <View style={styles.top}>
+            <Text style={styles.question}>
+              Q: {questions[currentQuestion].question}
+            </Text>
+          </View>
+          {/* Options section */}
+          <View style={styles.options}>
+            {questions[currentQuestion].incorrect_answers.map(
+              (option, index) => (
+                <TouchableOpacity key={index} style={styles.optionButton}>
+                  <Text style={styles.option}>{option}</Text>
+                </TouchableOpacity>
+              ),
+            )}
+            <TouchableOpacity style={styles.optionButton}>
+              <Text style={styles.option}>
+                {questions[currentQuestion].correct_answer}
+              </Text>
+            </TouchableOpacity>
+          </View>
+          {/* Bottom buttons */}
+          <View style={styles.bottom}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Skip</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                setCurrentQuestion(currentQuestion + 1);
+              }}>
+              <Text style={styles.buttonText}>Next</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                navigation.navigate('Results');
+              }}>
+              <Text style={styles.buttonText}>End</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        // Displayed when there are no questions
+        <View style={styles.noQuestionsContainer}>
+          <Text style={styles.noQuestionsText}>
+            No questions available. Please try again later.
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -60,10 +97,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   bottom: {
-    marginBottom: 12,
+    marginBottom: 20,
     paddingVertical: 16,
     justifyContent: 'space-between',
     flexDirection: 'row',
+    alignItems: 'center',
   },
   button: {
     backgroundColor: '#1A759F',
@@ -71,7 +109,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     borderRadius: 16,
     alignItems: 'center',
-    marginBottom: 30,
   },
   buttonText: {
     fontSize: 18,
@@ -96,8 +133,13 @@ const styles = StyleSheet.create({
   parent: {
     height: '100%',
   },
-
-  Question: {
-    fontSize: 28,
+  noQuestionsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  noQuestionsText: {
+    fontSize: 20,
+    textAlign: 'center',
   },
 });
